82cfd93013d147e39a41e9f2bc869f019cb8b1fd
PqBenoit <benoit@monsieurdrive.com>
2014-05-22 18:43:36 +0200

    Setting phonegap

---
+++ scripts/main.js
@@ -0,0 +1,99 @@
+(function() {
+  var payetapinteApp;
+
+  payetapinteApp = angular.module('payetapinteApp', ['ngRoute', 'geolocation']);
+
+  payetapinteApp.config([
+    '$routeProvider', function($routeProvider) {
+      return $routeProvider.when('/', {
+        templateUrl: 'partials/home.html',
+        controller: 'MainCtrl'
+      }).when('/bars', {
+        templateUrl: 'partials/bars.html',
+        controller: 'MainCtrl'
+      });
+    }
+  ]);
+
+  payetapinteApp.controller('MainCtrl', [
+    '$scope', '$http', 'geolocation', function($scope, $http, geolocation) {
+      var i, latTab, lngTab;
+      latTab = [];
+      lngTab = [];
+      i = 0;
+      return $http.get('bars.json').success(function(data) {
+        $scope.bars = data;
+        while (i < data.length) {
+          latTab[i] = data[i].latitude;
+          lngTab[i] = data[i].longitude;
+          i++;
+        }
+        i = 0;
+        return geolocation.getLocation().then(function(data) {
+          var featuresOpts, iconUrl, input, mapOptions, marker, markerIcon, searchBox;
+          $scope.coords = {
+            lat: data.coords.latitude,
+            lng: data.coords.longitude
+          };
+          featuresOpts = [
+            {
+              featureType: "water",
+              stylers: [
+                {
+                  color: "#8bc6fd"
+                }
+              ]
+            }, {
+              featureType: "landscape",
+              stylers: [
+                {
+                  color: "#ffffff"
+                }
+              ]
+            }
+          ];
+          mapOptions = {
+            zoom: 14,
+            center: new google.maps.LatLng(data.coords.latitude, data.coords.longitude),
+            mapTypeId: google.maps.MapTypeId.TERRAIN,
+            styles: featuresOpts,
+            panControl: false,
+            zoomControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+            streetViewControl: false,
+            overviewMapControl: false
+          };
+          $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+          iconUrl = 'http://payetapinte.fr/assets/img/icons/marker.png';
+          markerIcon = new google.maps.MarkerImage(iconUrl, null, null, null, new google.maps.Size(34, 44));
+          while (i < $scope.bars.length) {
+            marker = new google.maps.Marker({
+              position: new google.maps.LatLng(latTab[i], lngTab[i]),
+              icon: markerIcon,
+              map: $scope.map
+            });
+            i++;
+          }
+          input = document.getElementById('searchbox-input');
+          searchBox = new google.maps.places.SearchBox(input);
+          return google.maps.event.addListener(searchBox, "places_changed", function() {
+            var bounds, places;
+            places = searchBox.getPlaces();
+            bounds = new google.maps.LatLngBounds();
+            
+				for (var i = 0, place; place = places[i]; i++) {
+					bounds.extend(place.geometry.location);
+				}
+				;
+            $scope.map.fitBounds(bounds);
+            return $scope.map.setZomm(15);
+          });
+        });
+      });
+    }
+  ]);
+
+}).call(this);
+
+//# sourceMappingURL=main.js.map
